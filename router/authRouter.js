c9598027c7b408aaa417202e9a396ca8b4ae2f53
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-08-19 10:19:23 +0600

    routs

--- router/authRouter.js
+++ router/authRouter.js
@@ -4,10 +4,7 @@ const {
   loginController,
 } = require("../controllers/authController");
 const createLimiter = require("../utils/ralelimiter");
-// const authMiddleware = require("../middlewares/authMiddleware");
-// const registrationController = require("../controllers/registrationController");
-// const loginController = require("../controllers/loginController");
-// const privateDataController = require("../controllers/privateDataController");
+
 const _ = express.Router();
 
 
